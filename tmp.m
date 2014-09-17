ffdf2ba85d38e030c80e263c5a90fe00b2b419cf
Eminemya <weiddoonngglai@gmail.com>
2014-09-17 10:33:03 -0400

    organize

--- tmp.m
+++
@@ -1,3 +0,0 @@
-nn='100Hz_1000fps_ex_990us_l-FIRWindowBP-band99.00-101.00-sr1000-alpha100-mp1-sigma2-scale1.00-frames1-60-octave-0-60-1.avi';
-aa=VideoReader(nn);tmp_vr=aa.read();
-save amp_vid tmp_vr
